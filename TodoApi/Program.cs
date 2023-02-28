886f825b3c673c075d201d3542443245baf58852
Markus Thulin <macky_b@hotmail.com>
2023-02-28 17:35:48 +0100

    Enable Swagger

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -2,21 +2,30 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Reflection;
 using TodoApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
+var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(opt =>
+    opt.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename)));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (builder.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.UseDeveloperExceptionPage();
 }
 
